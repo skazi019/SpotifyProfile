cf2bc18f857756bc091b079612ea786cd5a9c04b
Kaushal Sharma <skazi019@gmail.com>
2022-06-13 13:35:50 +0530

    setting changes

--- spotify_v1/settings.py
+++ spotify_v1/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # DIRS for development
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 # ROOT for Production
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
